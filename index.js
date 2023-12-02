30e61d6c211a17625e0fc0dbe6648ffd5f773213
Bhairav <bhairavgotam066@gmail.com>
2023-12-02 11:25:56 +0530

    added database in index.js file and .env update

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require("express");
-
+const cors = require("cors");
+const { connection } = require("./config/db");
 const app = express()
 
+require("dotenv").config()
 app.get("/",(req,res)=>{
     res.send({msg:"Book Library Service"})
 })
 
 
 
-app.listen(8080,()=>{
-    console.log({msg:"server is running on port 8080"})
+app.listen(process.env.PORT,async()=>{
+    try {
+        await connection
+        console.log({msg:"connected to DB"})
+    } catch (error) {
+        console.log({error:error.message})
+    }
+    console.log({msg:`server is running on port ${process.env.PORT}`})
 })
